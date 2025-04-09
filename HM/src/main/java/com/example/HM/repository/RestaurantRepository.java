c5a8f34185bce7d560de1ef992cea0cd3ff6d85f
M10D12 <107938456+M10D12@users.noreply.github.com>
2025-04-09 21:01:15 +0100

    Delete HM directory

--- HM/src/main/java/com/example/HM/repository/RestaurantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.HM.repository;
-import com.example.HM.model.Restaurant;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
-
-    
-}
